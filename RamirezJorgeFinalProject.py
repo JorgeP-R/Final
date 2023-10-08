430d111a253a3cda6278faf6e89d1efc63a37dec
JorgeP-R <117873544+JorgeP-R@users.noreply.github.com>
2023-10-08 13:19:22 -0400

    Add files via upload

--- RamirezJorgeFinalProject.py
+++ RamirezJorgeFinalProject.py
@@ -1,12 +1,12 @@
 """
 Author:  Jorge Ramirez
-Date written: 09/24/23
+Date written: 10/08/23
 Assignment:   Final Project
 Short Desc:   Loan Calculator 
 """
 import tkinter as tk
 from tkinter import messagebox
-
+#Establish the calculator and the boxes and labels and buttons
 class LoanCalculator:
     def __init__(self, root):
         self.root = root
@@ -38,7 +38,7 @@ class LoanCalculator:
 
         self.result_label = tk.Label(root, height=10, width=40)
         self.result_label.pack()
-
+#Do the calculations
     def calculate_loan(self):
         try:
             initial_amount = float(self.initial_amount_entry.get())
@@ -54,16 +54,36 @@ class LoanCalculator:
                 result_text += f"{year}\t${initial_amount:.2f}\t${interest_paid:.2f}\n"
 
             self.result_label.config(text=result_text + f"Total Interest Paid on your Loan: ${total_interest:.2f}")
+#If not valid data display error
         except ValueError:
             messagebox.showerror("Error", "Invalid input. Please enter numeric values.")
-
+#Clear button 
     def clear_entries(self):
         self.initial_amount_entry.delete(0, tk.END)
         self.years_entry.delete(0, tk.END)
         self.interest_rate_entry.delete(0, tk.END)
         self.result_label.config(text="")
+#Second window
+class SecondWindow:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Second Window")
+
+        self.root.geometry("400x300")
+        
+        self.second_window_label = tk.Label(root, text="This is the second window.")
+        self.second_window_label.pack()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
     calculator = LoanCalculator(root)
+    
+    
+    second_window = tk.Toplevel(root)
+    second_window.title("Second Window")
+    second_window_app = SecondWindow(second_window)
+
+
+
     root.mainloop()
